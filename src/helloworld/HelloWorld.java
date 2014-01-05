55991c535aef5b6692113b48bf6416823a703774
ITSC <ITSC@192.168.0.112>
2014-01-05 18:28:48 -0500

    2nd Check in of HelloWorld

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -9,5 +9,7 @@ public class HelloWorld
     public static void main(String[] args)
     {
         System.out.println("Hello World!!!");
+        int var1 = 123;
+        System.out.println(var1);
     }
 }
